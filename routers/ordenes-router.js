1d6a931a60cccc96ea298c857af7112bd33d5a89
JAzzad5 <carlosjosue0114@gmail.com>
2021-11-10 00:27:33 -0600

    [ADD] ver ordenes por usuario [FIX] datos del comercio y el usuario de la orden

--- routers/ordenes-router.js
+++ routers/ordenes-router.js
@@ -3,13 +3,42 @@ var router = express.Router();
 var mongoose = require('mongoose');
 const pruebaOrden = require('../models/pruebaOrden');
 
+
+//obtener todas las ordenes
 router.get('/', function( req, res ){
-    pruebaOrden.find({})
+    pruebaOrden.find()
     .populate('usuario')
     .populate(
         {
             path:'productos',
             populate:{path:'_id'}
+        }
+    )
+    .then( result =>{
+        res.send(result);
+        res.end();
+    })
+    .catch( error =>{
+        res.send(error);
+        res.end();
+    });
+});
+
+
+//obtener ordenes de usuario
+router.get('/:idUsuario', function( req, res ){
+    pruebaOrden.find({usuario: mongoose.Types.ObjectId(req.params.idUsuario)})
+    .populate({path:'usuario', select: 'NombreUsuario Ubicacion'})
+    .populate(
+        {
+            path:'productos',
+            populate:{
+                path:'_id',
+                populate:{
+                    path:'Comercio',
+                    select: 'NombreComercio ImagenComercio Direccion'
+                }
+            }
         }
     )
     .then( result =>{
